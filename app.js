d1e6b62412306f6e7322549fbb7963bf2c49991a
Kenny T <ktrionfo@integracore.com>
2016-01-08 21:30:13 +0000

    added a button to home page

--- app.js
+++ app.js
@@ -2,3 +2,28 @@
 var beatherApp = angular.module('beatherApp', ['ngRoute', 'ngResource']
 
 );
+
+/////////////////////////========CONTROLLERS=======\\\\\\\\\\\\\\\\\\\\\\\\\\\
+beatherApp.controller('homeController', ['$scope', function($scope){
+
+}]);
+
+beatherApp.controller('forecastController', ['$scope', function($scope){
+
+}]);
+
+/////////////////////////========ROUTES=======\\\\\\\\\\\\\\\\\\\\\\\\\\\
+beatherApp.config(function ($routeProvider){
+	$routeProvider
+	.when('/', {
+		templateUrl: 'pages/home.htm',
+		controller: 'homeController'
+	})
+	.when('/forecast', {
+		templateUrl: 'pages/forecast.htm',
+		controller: 'forecastController'
+	})
+
+})
+
+
