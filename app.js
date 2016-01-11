57d4ed8a4f05c5c1826715862ba25b62b7011d47
Kenny T <ktrionfo@integracore.com>
2016-01-11 21:51:13 +0000

    added pages folder

--- app.js
+++ app.js
@@ -4,11 +4,35 @@ var beatherApp = angular.module('beatherApp', ['ngRoute', 'ngResource']
 );
 
 /////////////////////////========CONTROLLERS=======\\\\\\\\\\\\\\\\\\\\\\\\\\\
-beatherApp.controller('homeController', ['$scope', function($scope){
-
+beatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService){
+	$scope.city = cityService.city;
+	$scope.$watch('city', function(){
+		cityService.city = $scope.city;
+	});
 }]);
 
-beatherApp.controller('forecastController', ['$scope', function($scope){
+beatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService){
+
+	$scope.city = cityService.city;
+	$scope.days = $routeParams.days || '2'; 				
+	$scope.weatherApi = 
+		$resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=d8f5102c89d08caf442ba64a6bcda871", 
+			{
+				callback: "JSON_CALLBACK" //this and the method below just says "it's ok to get this data. it's not a hack attempt."
+			},
+			{
+				get: 
+					{
+						method: "JSONP"
+					}
+			}
+		);
+		$scope.weatherResult = $scope.weatherApi.get( 
+			{
+				q: $scope.city,
+				cnt: $scope.days 
+			});
+		console.log($scope.weatherResult);
 
 }]);
 
@@ -23,7 +47,16 @@ beatherApp.config(function ($routeProvider){
 		templateUrl: 'pages/forecast.htm',
 		controller: 'forecastController'
 	})
+	.when('/forecast/:days', {
+		templateUrl: 'pages/forecast.htm',
+		controller: 'forecastController'
+	});	
+});
 
-})
+/////////////////////////========SERVICES=======\\\\\\\\\\\\\\\\\\\\\\\\\\\
+beatherApp.service('cityService', function() {
+	this.city = "Del Ray, FL";
+});
+
 
 
